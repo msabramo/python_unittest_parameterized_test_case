f31743515111448bcc594a76dde8fc081d5d29f2
Marc Abramowitz <marc@marc-abramowitz.com>
2012-08-24 13:01:02 -0700

    Add setup.py info

--- setup.py
+++ setup.py
@@ -12,14 +12,30 @@ long_description = long_description.replace('Example usage:\n\n.. code:: python'
 
 setup(
     name='parameterizedtestcase',
-    version='0.0.0',
+    version='0.0.1',
     description="""Parameterized tests for Python's unittest module""",
     long_description=long_description,
+    keywords='unittest',
     author='Marc Abramowitz',
     author_email='marc@marc-abramowitz.com',
     url='https://github.com/msabramo/python_unittest_parameterized_test_case',
     packages=['parameterizedtestcase'],
     test_suite='parameterizedtestcase.tests',
     use_2to3=True,
+    zip_safe=False,
     license='MIT',
+    classifiers=[
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.0',
+        'Programming Language :: Python :: 3.1',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Topic :: Software Development :: Testing',
+        'Natural Language :: English',
+        'Intended Audience :: Developers',
+    ]
 )
