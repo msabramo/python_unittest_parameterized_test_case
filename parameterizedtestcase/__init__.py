b8cf1f71a5f7bc25876d329bd7c1217dc2d1d3b9
Marc Abramowitz <marc@marc-abramowitz.com>
2014-01-31 08:24:12 -0800

    PEP8, cleanup

--- parameterizedtestcase/__init__.py
+++ parameterizedtestcase/__init__.py
@@ -3,7 +3,6 @@ try:
 except ImportError:
     import unittest
 
-# from functools import partial
 from functools import wraps
 
 
@@ -17,10 +16,13 @@ def get_decorated_method_name(method_name, param_names, param_values):
     return '%s_%s' % (method_name, '_'.join(name_components))
 
 
-def augment_method_docstring(method, new_class_dict, classname, param_names, param_values, new_method):
-    param_assignments_str = '; '.join(['%s = %s' % (k, v) for (k, v) in zip(param_names, param_values)])
-    extra_doc = "%s (%s.%s) [with %s] " \
-        % (method.__name__, new_class_dict.get('__module__', '<module>'), classname, param_assignments_str)
+def augment_method_docstring(method, new_class_dict, classname,
+                             param_names, param_values, new_method):
+    param_assignments_str = '; '.join(
+        ['%s = %s' % (k, v) for (k, v) in zip(param_names, param_values)])
+    extra_doc = "%s (%s.%s) [with %s] " % (
+        method.__name__, new_class_dict.get('__module__', '<module>'),
+        classname, param_assignments_str)
 
     try:
         new_method.__doc__ = extra_doc + new_method.__doc__
@@ -35,26 +37,31 @@ class ParameterizedTestCaseMetaClass(type):
 
         for attr_name, attr_value in class_dict.items():
             if callable(attr_value) and hasattr(attr_value, 'param_names'):
-                # print("Processing attr_name = %r; attr_value = %r" % (attr_name, attr_value))
+                # print("Processing attr_name = %r; attr_value = %r" % (
+                #     attr_name, attr_value))
 
                 method = attr_value
                 param_names = attr_value.param_names
                 data = attr_value.data
 
-                meta.process_method(classname, method, param_names, data, new_class_dict)
+                meta.process_method(
+                    classname, method, param_names, data, new_class_dict)
             else:
                 new_class_dict[attr_name] = attr_value
 
         return type.__new__(meta, classname, bases, new_class_dict)
 
     @classmethod
-    def process_method(cls, classname, method, param_names, data, new_class_dict):
+    def process_method(
+            cls, classname, method, param_names, data, new_class_dict):
         for param_values in data:
             new_method = cls.new_method(method, param_values)
-            new_method.__name__ = get_decorated_method_name(method.__name__, param_names, param_values)
-            augment_method_docstring(method, new_class_dict, classname, param_names, param_values, new_method)
+            new_method.__name__ = get_decorated_method_name(
+                method.__name__, param_names, param_values)
+            augment_method_docstring(
+                method, new_class_dict, classname,
+                param_names, param_values, new_method)
             new_class_dict[new_method.__name__] = new_method
-
 
     @classmethod
     def new_method(cls, method, param_values):
